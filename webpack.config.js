d2fd607393bedd309c35738ef0de54891f2db5f2
Nicholas Krul <nicholas.krul@gmail.com>
2018-07-25 08:55:38 +0100

    Javascript Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,27 @@
 const path = require('path');
 var webpack = require("webpack");
 
-//multi site configuration
+// multi site configuration
 module.exports = {
-	entry: {
-		site: './src/main/js/main.jsx',
+	entry : {
+		site : './src/main/js/main.jsx',
 	},
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname  + '/generatedsrc/main/resources/template/')
+	output : {
+		filename : '[name].js',
+		path : path.resolve(__dirname
+				+ '/generatedsrc/main/resources/template/')
 	},
-	module: {
-		loaders: [
-			{
-				test: /.jsx$/,
-				loaders: 'buble-loader',
-				include: path.join(__dirname, 'src/main/js'),
-				query: {
-					//objectAssign: 'Object.assign'
-				}
-			},
-			{
-			    test: /\.css$/,
-			    loaders: [
-			      'style-loader',
-			      'css-loader'
-			    ]
-			  }
-		]
+	module : {
+		rules : [ {
+			test : /\.(js|jsx)$/,
+			exclude : /node_modules/,
+			use : [ 'babel-loader' ]
+		}, {
+			test : /\.css$/,
+			use : [ 'style-loader', 'css-loader' ]
+		} ]
+	},
+	resolve : {
+		extensions : [ '*', '.js', '.jsx' ]
 	}
 };
